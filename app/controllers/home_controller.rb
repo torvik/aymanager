35c9923c4862904489662cf6b498b2a8c9f707d8
torvik <pless.victor@gmail.com>
2018-04-03 20:34:10 +0000

    alterações home - falta revisar o bootstrap

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,36 @@
 class HomeController < ApplicationController
   def index
+    @totalhoras =  Improvement.all.sum(:horas_orca)
+
+    @totalepts = Improvement.count
+
+    @maioravanco = Improvement.maximum(:avanco)
+    @menoravanco = Improvement.minimum(:avanco)
+
+    @maiororcamento = Improvement.maximum(:horas_orca)
+    @menororcamento = Improvement.minimum(:horas_orca)
+
+    @status = Status.where(name: "Definindo escopo")
+    @total_ept_defesc = Improvement.where(status_id: @status).count
+
+    @status = Status.where(name: "Aprovação do orçamento")
+    @total_ept_aprov = Improvement.where(status_id: @status).count
+
+    @status = Status.where(name: "Valorando")
+    @total_ept_valor = Improvement.where(status_id:  @status).count
+    
+    @status = Status.where(name: "Construção")
+    @total_ept_const = Improvement.where(status_id: @status).count
+    
+    @status = Status.where(name: "Entregue para testes")
+    @total_ept_teste = Improvement.where(status_id: @status).count
+    
+    @status = Status.where(name: "Aguardando análise")
+    @total_ept_aguard = Improvement.where(status_id: @status).count
+    
+    @status = Status.where(name: "Em Produção")
+    @total_ept_prod = Improvement.where(status_id: @status).count
+
+
   end
 end
